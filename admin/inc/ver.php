9549e1cfed38467994d6e71896d338c5be71aec0
XianzhiChris <godwu2846@163.com>
2016-07-19 06:35:35 +0800

    更新版本号1.2.3

--- admin/inc/ver.php
+++ admin/inc/ver.php
@@ -1,5 +1,5 @@
 <?php
-define('VER','1.2.2');
-define('VER_DATE','2016/07/18');
+define('VER','1.2.3');
+define('VER_DATE','2016/07/19');
 define('QQ','交流反馈:<a target="_blank" href="http://shang.qq.com/wpa/qunwpa?idkey=18f9fcd3ee4db086cbebc448ea900a0b0ad9b0388f170fd52268a87c7f5b2866"><img border="0" src="http://pub.idqqimg.com/wpa/images/group.png" alt="阿里蜘蛛池" title="阿里蜘蛛池"></a>')
 ?>
