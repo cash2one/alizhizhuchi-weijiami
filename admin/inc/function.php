e8a0beba8b1e9d4ac39a308d8006c31d585bcaf3
XianzhiChris <godwu2846@163.com>
2016-07-24 14:31:59 +0800

    增加curl扩展验证

--- admin/inc/function.php
+++ admin/inc/function.php
@@ -446,34 +446,39 @@ function spider_type_list(){
     return $str;
 }
 function request_post($post_data = array()) {
-    $url="http://vip.xianzhihulian.com/index.php";
-    $duankou=$_SERVER["SERVER_PORT"];
-    $yuming=$_SERVER['HTTP_HOST'];
-    $yuming=str_replace(':'.$duankou, '', $yuming);
-    $post_data['domain']=$yuming;
-    if (empty($url) || empty($post_data)) {
-        return false;
+    if(function_exists("curl_init")){
+        $url="http://vip.xianzhihulian.com/index.php";
+        $duankou=$_SERVER["SERVER_PORT"];
+        $yuming=$_SERVER['HTTP_HOST'];
+        $yuming=str_replace(':'.$duankou, '', $yuming);
+        $post_data['domain']=$yuming;
+        if (empty($url) || empty($post_data)) {
+            return false;
+        }
+
+        $o = "";
+        foreach ( $post_data as $k => $v )
+        {
+            $o.= "$k=" . urlencode( $v ). "&" ;
+        }
+        $post_data = substr($o,0,-1);
+
+        $postUrl = $url;
+        $curlPost = $post_data;
+        $ch = curl_init();//初始化curl
+        curl_setopt($ch, CURLOPT_URL,$postUrl);//抓取指定网页
+        curl_setopt($ch, CURLOPT_HEADER, 0);//设置header
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);//要求结果为字符串且输出到屏幕上
+        curl_setopt($ch, CURLOPT_POST, 1);//post提交方式
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $curlPost);
+        $data = curl_exec($ch);//运行curl
+        curl_close($ch);
+
+        return $data;
+    }else{
+        echo "请在服务器开启curl扩展";
+        exit;
     }
-
-    $o = "";
-    foreach ( $post_data as $k => $v )
-    {
-        $o.= "$k=" . urlencode( $v ). "&" ;
-    }
-    $post_data = substr($o,0,-1);
-
-    $postUrl = $url;
-    $curlPost = $post_data;
-    $ch = curl_init();//初始化curl
-    curl_setopt($ch, CURLOPT_URL,$postUrl);//抓取指定网页
-    curl_setopt($ch, CURLOPT_HEADER, 0);//设置header
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);//要求结果为字符串且输出到屏幕上
-    curl_setopt($ch, CURLOPT_POST, 1);//post提交方式
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $curlPost);
-    $data = curl_exec($ch);//运行curl
-    curl_close($ch);
-
-    return $data;
 }
 //获取配置信息
 function config_list(){
