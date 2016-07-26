0e4318cf1d5bc2ed7ecf04910dd6a2623e07f810
XianzhiChris <godwu2846@163.com>
2016-07-26 12:09:04 +0800

    去掉curl超时限制

--- admin/inc/function.php
+++ admin/inc/function.php
@@ -468,7 +468,7 @@ function request_post($post_data = array()) {
         $ch = curl_init();//初始化curl
         curl_setopt($ch, CURLOPT_URL,$postUrl);//抓取指定网页
         curl_setopt($ch, CURLOPT_HEADER, 0);//设置header
-        curl_setopt($ch, CURLOPT_TIMEOUT,3);
+        //curl_setopt($ch, CURLOPT_TIMEOUT,3);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);//要求结果为字符串且输出到屏幕上
         curl_setopt($ch, CURLOPT_POST, 1);//post提交方式
         curl_setopt($ch, CURLOPT_POSTFIELDS, $curlPost);
